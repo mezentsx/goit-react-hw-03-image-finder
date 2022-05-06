5672bba151aed55ccf1ab9fed382406791b9d6d3
Xeniya Mezentseva <mezentsxn@gmail.com>
2022-05-06 19:25:18 +0200

    fixed

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import s from "./ImageGalleryItem.module.css";
 
-class ImageGalleryItem extends Component {
-  onImageClick = () => {
-    this.props.onClick(this.props.largeImageURL, this.props.tags);
+const ImageGalleryItem = (props) => {
+  const onImageClick = () => {
+    props.onClick(props.largeImageURL, props.tags);
   };
 
-  render() {
-    const { id, webformatURL, tags } = this.props;
+  const { id, webformatURL, tags } = props;
 
-    return (
-      <li className={s.ImageGalleryItem}>
-        <img
-          className={s.ImageGalleryItemImage}
-          id={id}
-          src={webformatURL}
-          alt={tags}
-          onClick={this.onImageClick}
-        />
-      </li>
-    );
-  }
-}
+  return (
+    <li className={s.ImageGalleryItem}>
+      <img
+        className={s.ImageGalleryItemImage}
+        id={id}
+        src={webformatURL}
+        alt={tags}
+        onClick={onImageClick}
+      />
+    </li>
+  );
+};
 
 export default ImageGalleryItem;
